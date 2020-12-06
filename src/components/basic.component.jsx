ad21933f06cf0e4fa74e20de011ba1270879edd2
silentunion <milliner@unbc.ca>
2020-12-06 09:36:36 -0800

    Added axios/connection to local API

--- src/components/basic.component.jsx
+++ src/components/basic.component.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import axios from 'axios';
 
 import testNames from './testNames.json';
 
 export default function BasicText(value) {
-    const [data, setData] = useState(testNames.names);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:8666/letters`)
+            .then((res) => setData(res.data))
+    }, []);
 
     return(
         <div>
             {console.log(data)}
-            {data.map(({name}) => (
-                <p>{name}</p>
+            {data.map(({letter}) => (
+                <p>{letter}</p>
             ))}
         </div>
     );
