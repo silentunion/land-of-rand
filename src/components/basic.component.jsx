59ebed92a4dfd442cb3afadf67de1cf1c9a91f4a
silentunion <milliner@unbc.ca>
2020-12-07 08:30:03 -0800

    Moved exclusive sass folders to a parent sass folder

--- src/components/basic.component.jsx
+++ src/components/basic.component.jsx
@@ -8,17 +8,20 @@ import testNames from './testNames.json';
 export default function BasicText(value) {
     const [data, setData] = useState([]);
 
+    const template = 'cvccv'
+    const num = 6
+
     useEffect(() => {
         axios
-            .get(`http://localhost:8666/letters`)
+            .get(`http://localhost:5000/words/${template}/${num}`)
             .then((res) => setData(res.data))
     }, []);
 
     return(
         <div>
             {console.log(data)}
-            {data.map(({letter}) => (
-                <li key={letter}>{letter}</li>
+            {data.map(({word}) => (
+                <li key={word}>{word}</li>
             ))}
         </div>
     );
