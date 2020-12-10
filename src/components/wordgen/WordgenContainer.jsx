e9eb52f2fd66e8d1ce699814ed2cf112f6a8afab
silentunion <milliner@unbc.ca>
2020-12-10 08:07:01 -0800

    Created a 'completely random' button that connects to a new endpoint

--- src/components/wordgen/WordgenContainer.jsx
+++ src/components/wordgen/WordgenContainer.jsx
@@ -9,23 +9,38 @@ import WordView from './wordview.component';
 const WordgenContainer = () => {
   const [data, setData] = useState([]);
   const [words, setWords] = useState([]);
-
+  const [endpoint, setEndpoint] = useState(0);
+  
   useEffect(() => {
-    if (words.num && words.template) {
+    if (endpoint === 0) {
+      setData([{'word': ''}])
+    } else if (endpoint === 1) {
       axios
         .get(`http://localhost:5000/words/${words.num}/${words.template}`)
+        .then((res) => setData(res.data))
+    } else if (endpoint === 2) {
+      axios
+        .get(`http://localhost:5000/words/random/${words.num}`)
         .then((res) => setData(res.data))
     }
   }, [words]);
 
+  const handleEvent = event => {
+    setEndpoint(2);
+    setWords({'num': 5});
+  };
+
   const changeTemplate = (formData) => {
+    setEndpoint(1);
     setWords(formData);
   };
 
   return (
   <div className="wordgen-container">
     <WordMenu />
-    <WordForm onceSubmitted={(formData) => changeTemplate(formData)} />
+    <WordForm 
+      onClickRandom={handleEvent}
+      onceSubmitted={(formData) => changeTemplate(formData)} />
     <WordView data={data} />
   </div>
   );
--- src/components/wordgen/wordform.component.jsx
+++ src/components/wordgen/wordform.component.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
-const WordForm = ({onceSubmitted}) => {
-  const { register, handleSubmit } = useForm();
+const WordForm = ({onceSubmitted, onClickRandom}) => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      template: "vcvcvc",
+      num: 1
+    }
+  });
 
   const onSubmit = (data) => {
+    register()
     onceSubmitted(data);
-  }
+  };
+
+  const handleEvent = event => {
+    onClickRandom(event);
+  };
 
   return(
     <div className="wordform-scrollbox">
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>Template:</label>
-        <input type="text" name="template" defaultValue="vcvcv" ref={register} />
+        <input type="text" name="template" ref={register} />
         <label>Number of Words:</label>
-        <input type="number" name="num" min="1" defaultValue="1" ref={register} />
+        <input type="number" name="num" min="1" ref={register} />
         <input type="submit" name="Submit" />
       </form>
+      <button onClick={handleEvent} class="button-random">
+        Completely Random
+      </button>
     </div>
   );
 };
