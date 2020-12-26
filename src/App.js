efb7875b3195240009f8376fbb22136ba6549e0e
silentunion <milliner@unbc.ca>
2020-12-26 10:50:20 -0800

    Added extra space component for testing purposes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './main.scss';
 import Header from './components/Header/Header';
 import NavBar from './components/Header/Navbar';
 import NamegenContainer from './components/namegen/NamegenContainer';
+import ExtraSpace from './components/ExtraSpace/ExtraSpace';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <NamegenContainer />
+      <ExtraSpace />
     </div>
   );
 }
