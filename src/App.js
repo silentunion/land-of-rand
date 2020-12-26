1a3a280a7eaeb08d4f87ec5af8ea958873730c3b
silentunion <milliner@unbc.ca>
2020-12-26 09:46:48 -0800

    Moved navbar items to header folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './main.scss';
 
-import Header from './components/header/Header';
-import NavBar from './components/navbar/Navbar';
+import Header from './components/Header/Header';
+import NavBar from './components/Header/Navbar';
 import NamegenContainer from './components/namegen/NamegenContainer';
 
 function App() {
