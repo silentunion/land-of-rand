c46e81065c98c041a40cc923663bb33621394ef4
silentunion <milliner@unbc.ca>
2020-12-26 09:25:40 -0800

    Added temporary background image/title

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header>
       <div className="top-section"></div>
       <div className="main-section">
-        <h1>Land of Rand</h1>
+        <div className="title"><h1>Land of Rand</h1></div>
         <p>Some random quote</p>
       </div>
       <div className="bottom-section"></div>
