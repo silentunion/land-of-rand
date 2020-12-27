a18b81a2e3bae7dd1eb633b40e5dfc58098a96b7
silentunion <milliner@unbc.ca>
2020-12-27 07:33:18 -0800

    Created initial pages to be used in react router

---
+++ src/pages/names.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NamegenContainer from '../components/namegen/NamegenContainer';
+import ExtraSpace from '../components/ExtraSpace/ExtraSpace';
+
+const Names = () => {
+  return (
+    <div>
+      <NamegenContainer />
+      <ExtraSpace />
+    </div>
+  )
+};
+
+export default Names;
