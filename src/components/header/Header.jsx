9b23ec054723b2dc9f02ee28af1905398a0ce1f2
silentunion <milliner@unbc.ca>
2020-12-27 07:26:50 -0800

    Downloaded and installed react router dom

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 const Header = () => {
   const [open, setOpen] = useState(false);
 
-
   return (
     <div>
       <header>
@@ -15,7 +14,7 @@ const Header = () => {
         <div className="bottom-section"></div>
       </header>
       <nav>
-        <div className="logo">Land of Rand</div>
+        {/* <div className="logo">Land of Rand</div> */}
         <ul className="nav-links" 
           style={{transform: open ? "translateY(4rem)" : ""}}>
           <li><a href="home">Home</a></li>
