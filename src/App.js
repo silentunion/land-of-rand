3dad211c77540eb92684389b341af3d344483d3d
silentunion <milliner@unbc.ca>
2020-12-27 08:15:00 -0800

    Navbar now sticks

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div className="sticky-nav" ><NavBar /></div>
       <NamegenContainer />
       <ExtraSpace />
     </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,16 +13,6 @@ const Header = () => {
         </div>
         <div className="bottom-section"></div>
       </header>
-      <nav>
-        {/* <div className="logo">Land of Rand</div> */}
-        <ul className="nav-links" 
-          style={{transform: open ? "translateY(4rem)" : ""}}>
-          <li><a href="home">Home</a></li>
-          <li><a href="words">Words</a></li>
-          <li><a href="about">About</a></li>
-        </ul>
-          <i onClick={() => setOpen(!open)} className="nav-burger fas fa-bars" />
-      </nav>
     </div>
   )
 };
